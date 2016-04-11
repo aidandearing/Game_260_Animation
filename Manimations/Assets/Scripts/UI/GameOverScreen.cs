e41b323d97cb1c70111e3d9c8f7100bb8026fca1
matbecker <7m4tt3r7@gmail.com>
2016-04-10 22:18:19 -0400

    added different game states along with the pause menu

---
+++ Manimations/Assets/Scripts/UI/GameOverScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameOverScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	public void onRestart()
+	{
+		SceneManager.LoadScene("Game");
+	}
+	public void onQuit()
+	{
+		SceneManager.LoadScene("StartScreen");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
