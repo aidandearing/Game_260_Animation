090967fdfb88ca8be57fbee0bc4e7b0a1d2037a2
matbecker <7m4tt3r7@gmail.com>
2016-04-17 16:40:39 -0400

    added destructable garbage cans and barrels

---
+++ Manimations/Assets/Scripts/Destructable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destructable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.collider.CompareTag("Hit"))
+		{
+			Destroy(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
