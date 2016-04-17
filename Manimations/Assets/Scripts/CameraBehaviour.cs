0afc94831f8e34e02e7a4b07c3b667ed1b72dcbd
matbecker <7m4tt3r7@gmail.com>
2016-04-17 18:12:42 -0400

    camera class and 2nd player

---
+++ Manimations/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	GameObject[] players;
+	// Use this for initialization
+	void Start () {
+		players = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 com = Vector3.zero;
+
+		foreach (GameObject player in players)
+		{
+			com += player.transform.position;
+		}
+		com /= players.Length;
+
+		float distFromCom = float.NegativeInfinity;
+
+		foreach(GameObject player in players)
+		{
+			distFromCom = Mathf.Max((com - player.transform.position).magnitude, distFromCom);
+		}
+
+		transform.position = com + new Vector3(5 + distFromCom,5 + distFromCom,0);
+	
+	}
+}
--- Manimations/Assets/Scripts/Player/SpawnPoint.cs
+++ Manimations/Assets/Scripts/Player/SpawnPoint.cs
@@ -4,16 +4,34 @@ using System.Collections;
 public class SpawnPoint : MonoBehaviour {
 
 	public PlayerController player;
+	public float timer;
+	public bool playerDead;
+	public float respawnAmount;
 	// Use this for initialization
 	void Start () 
 	{
+		playerDead = false;
 		player.transform.position = transform.position;
+		respawnAmount = 5.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		//if (player.)
-	
+		if (playerDead)
+		{
+			timer += Time.deltaTime;
+		}
+		if (timer > respawnAmount)
+		{
+			player.transform.position = transform.position;
+			timer = 0.0f; 
+			playerDead = false;
+		}
+
+	}
+	void Respawn()
+	{
+		playerDead = true;
 	}
 }
