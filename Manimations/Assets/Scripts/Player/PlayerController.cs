493073c2d7984bfa08af3a91ab24e1b24269bcb5
aidandearing <aidan.dearing@live.com>
2016-04-17 16:36:55 -0400

    More PlayerController Changes
    Attack, kinda, charge, kinda, block, and bash, maybe?

--- Manimations/Assets/Scripts/Player/PlayerController.cs
+++ Manimations/Assets/Scripts/Player/PlayerController.cs
@@ -8,10 +8,12 @@ public class PlayerController : MonoBehaviour
 
     // 10
     enum AnimatorParameters { walkingSpeed, isWalking, isAttacking, isBlocking, isGrabbing, isTaunting, isRunning, isAttacked, isStunned, isDead };
-    enum State { start, idle, walk, run, attack, block, dead };
+    enum State { start, idle, walk, run, attack, block, bash, charge, dead };
 
     State state = State.idle;
     State stateLast = State.start;
+
+    bool isAttacked = false;
 
     // Use this for initialization
     void Start()
@@ -35,6 +37,8 @@ public class PlayerController : MonoBehaviour
 
         UpdateInput();
         UpdateState();
+
+        isAttacked = false;
     }
 
     void UpdateInput()
@@ -44,32 +48,72 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             movement += new Vector3(0, 0, 1);
-            state = State.walk;
         }
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             movement += new Vector3(-1, 0, 0);
-            state = State.walk;
         }
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             movement += new Vector3(0, 0, -1);
-            state = State.walk;
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             movement += new Vector3(1, 0, 0);
-            state = State.walk;
+        }
+
+        if (movement.magnitude > 0)
+        {
+            movement.Normalize();
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                movement *= 5.0f;
+                state = State.run;
+            }
+            else
+            {
+                movement *= 1.5f;
+                state = State.walk;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Space) || Input.GetButton("Fire1"))
+        {
+            state = State.attack;
+        }
+
+        if (Input.GetKey(KeyCode.LeftAlt) || Input.GetButton("Fire2"))
+        {
+            if (state == State.attack)
+            {
+                state = State.bash;
+            }
+            else if (state == State.run)
+            {
+                state = State.charge;
+            }
+            else
+            {
+                state = State.block;
+            }
         }
 
         AnimatorStateInfo anim = selfAnimator.GetCurrentAnimatorStateInfo(0);
 
-        if (anim.IsName("Walk"))
+        if (anim.IsName("Walk") || anim.IsName("Run") || anim.IsName("Shield Charge"))
         {
-            movement.Normalize();
-            movement *= 1;
+            if (state == State.run && anim.IsName("Run"))
+            {
+                movement *= 3.0f;
+            }
+            else if (state == State.walk && anim.IsName("Walk"))
+            {
+                movement *= 1.5f;
+            }
 
             selfRigid.MovePosition(transform.position + movement * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.1f);
         }
     }
 
@@ -186,8 +230,52 @@ public class PlayerController : MonoBehaviour
                     selfAnimator.SetBool("isIdle", false);
                 }
                 break;
+            case State.bash:
+                // This is newly changed
+                if (state != stateLast)
+                {
+                    selfAnimator.SetFloat("movementSpeed", 0.0f);
+                    selfAnimator.SetBool("isWalking", false);
+                    selfAnimator.SetBool("isAttacking", true);
+                    selfAnimator.SetBool("isBlocking", true);
+                    selfAnimator.SetBool("isGrabbing", false);
+                    selfAnimator.SetBool("isTaunting", false);
+                    selfAnimator.SetBool("isRunning", false);
+                    selfAnimator.SetBool("isAttacked", false);
+                    selfAnimator.SetBool("isStunned", false);
+                    selfAnimator.SetBool("isDead", false);
+                    selfAnimator.SetBool("isIdle", false);
+                }
+                break;
+            case State.charge:
+                // This is newly changed
+                if (state != stateLast)
+                {
+                    selfAnimator.SetFloat("movementSpeed", 0.0f);
+                    selfAnimator.SetBool("isWalking", false);
+                    selfAnimator.SetBool("isAttacking", false);
+                    selfAnimator.SetBool("isBlocking", true);
+                    selfAnimator.SetBool("isGrabbing", false);
+                    selfAnimator.SetBool("isTaunting", false);
+                    selfAnimator.SetBool("isRunning", true);
+                    selfAnimator.SetBool("isAttacked", false);
+                    selfAnimator.SetBool("isStunned", false);
+                    selfAnimator.SetBool("isDead", false);
+                    selfAnimator.SetBool("isIdle", false);
+                }
+                break;
         }
 
         stateLast = state;
+    }
+
+    public void Attacked(PlayerController attacker)
+    {
+
+    }
+
+    void Attack()
+    {
+
     }
 }
