f6a7c7b28a34228795a279b66e01a505a1c08cf8
matbecker <7m4tt3r7@gmail.com>
2016-04-17 17:20:32 -0400

    spawn points yay

---
+++ Manimations/Assets/Scripts/Player/SpawnPoint.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour {
+
+	public PlayerController player;
+	// Use this for initialization
+	void Start () 
+	{
+		player.transform.position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//if (player.)
+	
+	}
+}
